ac5cefc1987469e4527d3ea45f11b0d69cf540fa
Sahil-Sam <itsmesahil357@gmail.com>
2024-07-30 16:34:22 +0530

    cleaned some code

--- src/services/tweet.ts
+++ src/services/tweet.ts
@@ -1,47 +1,46 @@
+import { json, text } from "body-parser";
 import { prismaClient } from "../client/db";
 import { redisClient } from "../client/redis";
 
 export interface CreateTweetPayload {
-  imageData: any;
-  content: string;
-  imageURL?: string; // Optional in the payload
-  userId: string;
-}
-
-class TweetService {
-  public static async createTweet(data: CreateTweetPayload) {
-    const rateLimitFlag = await redisClient.get(
-      `RATE_LIMIT:TWEET:${data.userId}`
-    );
-
-    if (rateLimitFlag) throw new Error("Please Wait...");
-
-    // Ensure imageURL is a string; provide a default value if undefined
-    const tweet = await prismaClient.tweet.create({
-      data: {
-        content: data.content,
-        imageURL: data.imageURL || '', // Default to empty string if undefined
-        author: { connect: { id: data.userId } },
-      },
-    });
-
-    await redisClient.setex(`RATE_LIMIT:TWEET:${data.userId}`, 10, "1");
-    await redisClient.del("ALL_TWEETS"); // Use del to clear the cache
-
-    return tweet;
+    imageData: any;
+    content: string;
+    imageURL?: string;
+    userId: string;
   }
+  
+  class TweetService {
+    public static async  createTweet(data: CreateTweetPayload) {
+      const rateLimitFlag = await redisClient.get(
+        `RATE_LIMIT:TWEET:${data.userId}`
+      );
 
-  public static async getAllTweets() {
-    const cachedTweets = await redisClient.get("ALL_TWEETS");
-    if (cachedTweets) return JSON.parse(cachedTweets);
+      if( rateLimitFlag ) throw new Error("Please Wait...");
+      const tweet = await prismaClient.tweet.create({
+        data: {
+          content: data.content,
+          imageURL: data.imageURL || '',
+          author: { connect: {id: data.userId,}},
+        },
+      });
+      await redisClient.setex( `RATE_LIMIT:TWEET:${data.userId}`,10, 1);
+      await redisClient.get("ALL_TWEETS");
+      return tweet
 
-    const tweets = await prismaClient.tweet.findMany({
-      orderBy: { createdAt: "desc" },
-    });
+    }
+  
+    public static async getAllTweets() {
+      const cachedTweets = await redisClient.get("ALL_TWEETS");
+      if (cachedTweets) return JSON.parse(cachedTweets);
 
-    await redisClient.set("ALL_TWEETS", JSON.stringify(tweets));
-    return tweets;
+
+      const tweets = await prismaClient.tweet.findMany({
+        orderBy: { createdAt: "desc", },
+      });
+      
+     await redisClient.set("ALL_TWEETS", JSON.stringify(tweets));
+     return tweets;JSON
+    }
   }
-}
-
-export default TweetService;
+  
+  export default TweetService;
