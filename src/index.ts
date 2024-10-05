94077d4cfdfb387d735774a267138edab109aa96
Sahil-Sam <itsmesahil357@gmail.com>
2024-10-05 19:05:01 +0530

    added some changes

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,34 @@
-import * as dotenv from "dotenv"
-import {initServer} from "./app"
+// import * as dotenv from "dotenv"
+// import {initServer} from "./app"
 
 
-dotenv.config();
+// dotenv.config();
  
 
 
-async function init (){
+// async function init (){
+//     const app = await initServer();
+//     app.listen(8000,()=>console.log("Server Started at PORT:8000"));
+
+// }
+// init();
+
+
+import * as dotenv from "dotenv";
+import { initServer } from "./app";
+
+dotenv.config();
+
+async function init() {
     const app = await initServer();
-    app.listen(8000,()=>console.log("Server Started at PORT:8000"));
 
+    // Add the route handler for the root path
+    app.get('/', (req, res) => {
+        res.send('Welcome to the API!'); // or whatever message you prefer
+    });
+
+    // Start the server on port 8000
+    app.listen(8000, () => console.log("Server Started at PORT:8000"));
 }
+
 init();
-
-
