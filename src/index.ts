82b8716b13a0d6182d1f9f74c5441c8fb20f214a
Sahil-Sam <itsmesahil357@gmail.com>
2024-10-05 17:56:24 +0530

    added some changes

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,16 @@
-// import * as dotenv from "dotenv"
-// import {initServer} from "./app"
+import * as dotenv from "dotenv"
+import {initServer} from "./app"
 
 
-// dotenv.config();
+dotenv.config();
  
 
 
-// async function init (){
-//     const app = await initServer();
-//     app.listen(8000,()=>console.log("Server Started at PORT:8000"));
+async function init (){
+    const app = await initServer();
+    app.listen(8000,()=>console.log("Server Started at PORT:8000"));
 
-// }
-// init();
-
-
-import * as dotenv from "dotenv";
-import { initServer } from "./app";
-
-dotenv.config();
-
-async function init() {
-  const app = await initServer();
-
-  // Use the PORT from the environment or default to 8000 for local development
-  const PORT = process.env.PORT || 8000;
-  
-  app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`));
 }
-
 init();
+
+
